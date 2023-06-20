47d062e72d332cdb01fe401647fc521e8fbe797d
K2054852 <k2054852@kingston.ac.uk>
2023-06-20 13:37:09 +0100

    Removed html

--- index.js
+++ index.js
@@ -12,23 +12,22 @@ app.use((req,res,next)=>{
     console.log(req.method,"-->",req.path)
     next()
 })
-app.use(express.static(__dirname+'/public'));
 
 app.use(modulesRouter);
 app.use(tasksRouter);
-app.get("/", (req,res)=>{
-    res.writeHead(200,{
-        'Content-Type': 'text/html'})
-    fs.readFile('./src/html/index.html', null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('File not found!');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
-});
+// app.get("/", (req,res)=>{
+//     res.writeHead(200,{
+//         'Content-Type': 'text/html'})
+//     fs.readFile('./src/html/index.html', null, function (error, data) {
+//         if (error) {
+//             res.writeHead(404);
+//             res.write('File not found!');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// });
 
 app.listen(PORT, console.log("Server successfuly started!"))
 
